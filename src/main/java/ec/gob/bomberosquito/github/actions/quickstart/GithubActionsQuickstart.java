ac228dd302ac78f09fe987305a8a40d6c474c6e2
Samuel Hwang <shwang@bomberosquito.gob.ec>
2023-07-10 23:12:23 -0500

    initial commit

---
+++ src/main/java/ec/gob/bomberosquito/github/actions/quickstart/GithubActionsQuickstart.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package ec.gob.bomberosquito.github.actions.quickstart;
+
+/**
+ *
+ * @author danielhwang
+ */
+public class GithubActionsQuickstart {
+
+    public static void main(String[] args) {
+        Dog mydog = new Dog("Rufo", 1);
+        System.out.println("My dog's name is " + mydog.getName());
+        System.out.println("My dog's age is " + mydog.getAge());
+    }
+}
